9a7982dd5713c9a7758bbd56bd0b53138aa5b351
Karamazov <samuel.silveira01@fatec.sp.gov.br>
2022-09-09 21:32:56 -0300

    apostila05: fim do bloco de codigo 2.6.1

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,37 @@ import React from 'react'
 
 class App extends React.Component {
 
+  constructor(props){
+    super(props)
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      estacao: null,
+      data: null,
+      icone: null,
+    }
+  }
+
+  obterEstacao( data, latitude) => {
+    const ano => data.getFullYear();
+    const d1 = new Date(ano, 5, 21)
+    const d2 = new Date(ano, 8, 24)
+    const d3 = new Date(ano, 11, 22)
+    const d4 = new Date(ano, 3, 21)
+    const sul = latitude < 0;
+    if (data >= d1 && date < d2) {
+      return sul ? 'Inverno' : 'Verão'
+    }
+    if (data >= d2 && data < d3) {
+      return sul ? 'Primavera' : 'Outono'
+    }
+    if (data >= d3 && data < d4) {
+      return sul ? 'Verão' : 'Inverno'      
+    }
+    return sul ? 'Outono' : 'Primavera'
+  }
+
   render() {
     return (
       <div>
